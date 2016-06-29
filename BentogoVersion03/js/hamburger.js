f4efe61cf3978ed1451ddef16846cee8e134ab27
lastblues <bluebomber9103@hotmail.com>
2016-06-29 05:21:26 -0700

    Added footer added new meta line added a new .css for the hamburger menu

--- BentogoVersion03/js/hamburger.js
+++ BentogoVersion03/js/hamburger.js
@@ -1,15 +1,15 @@
-$(document).read( function(){
+$(document).ready( function() {
 
 	var width = $(window).width();
 
-	if(width <352){
+	if (width < 320){
 		$(".nav-toggle").removeClass("hidden");//hide menu elements if screen size is less than 352 pix
 		$(".nav-toggle").addClass("shown");
 
 		$("nav").hide();
 	}
 
-	$(".nav=toggle").click( function() {
+	$(".nav-toggle").click( function() {
 		$("nav").slideToggle(500);
 
 	});
