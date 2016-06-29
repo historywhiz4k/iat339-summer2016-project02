94eb62578d52e765f996b090f554948bd0ab6083
lastblues <bluebomber9103@hotmail.com>
2016-06-29 13:44:03 -0700

    made hamburger menu appear next to logo in mobile jquery kind of works atm

--- BentogoVersion03/js/hamburger.js
+++ BentogoVersion03/js/hamburger.js
@@ -1,17 +1,26 @@
-$(document).ready( function() {
+$(document).ready(
+	function() {
+
+	console.log("jQuery is ready!");
 
 	var width = $(window).width();
+	console.log("Window width = "+width);
 
-	if (width < 320){
+	if (width < 430){
+
 		$(".nav-toggle").removeClass("hidden");//hide menu elements if screen size is less than 352 pix
 		$(".nav-toggle").addClass("shown");
 
-		$("nav").hide();
+		$(".button-nav").hide();
 	}
 
-	$(".nav-toggle").click( function() {
-		$("nav").slideToggle(500);
+	$(".nav-toggle").click( 
+		function() {
+			$(".grid-navbuttons").slideToggle(500);
 
-	});
+		}
 
-});
+	);
+}
+
+);
