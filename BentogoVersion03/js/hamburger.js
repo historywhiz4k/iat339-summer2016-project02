096b5f1e8c223b08a3d4bcf13596b5ab7df3f2ee
historywhiz4k <historywhiz4k@gmail.com>
2016-06-29 17:24:52 -0700

    Made changes to the drop down navigation, and some small layout changes.

--- BentogoVersion03/js/hamburger.js
+++ BentogoVersion03/js/hamburger.js
@@ -6,17 +6,17 @@ $(document).ready(
 	var width = $(window).width();
 	console.log("Window width = "+width);
 
-	if (width < 430){
+	if (width < 1000){
 
 		$(".nav-toggle").removeClass("hidden");//hide menu elements if screen size is less than 352 pix
 		$(".nav-toggle").addClass("shown");
 
-		$(".button-nav").hide();
+		$("nav").hide();
 	}
 
 	$(".nav-toggle").click( 
 		function() {
-			$(".grid-navbuttons").slideToggle(500);
+			$("nav").slideToggle(500);
 
 		}
 
