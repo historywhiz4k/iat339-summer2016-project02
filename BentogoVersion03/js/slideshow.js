94b29a40ffe527fdfe56cbd2d03c783c9a59c70f
lastblues <bluebomber9103@hotmail.com>
2016-06-30 05:03:44 -0700

    Readjusted some of the @media queries for the image texts on the front page. Having weird problem where <p> for the 4th picture is being placed on top of the first image and are fixed.

--- BentogoVersion03/js/slideshow.js
+++ BentogoVersion03/js/slideshow.js
@@ -2,17 +2,25 @@ $(document).ready(
   
   function() {
        var slideIndex = 1;
-showDivs(slideIndex);
+        showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+
+    if (n > x.length) 
+      {
+        slideIndex = 1
+      }
+
+    if (n < 1) 
+    {
+      slideIndex = x.length
+    }
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
